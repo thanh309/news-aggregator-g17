8a345f878f14cbb054ea96e6c8f4ba6aaf9134ad
thanh309 <149485828+thanh309@users.noreply.github.com>
2024-03-28 01:28:54 +0700

    test Selenium (#2)
    * test Selenium
    
    * small changes

---
+++ src/main/java/org/example/TestSelenium.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestSelenium {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+
+        driver.get("https://a16zcrypto.com/posts/?format=article");
+
+
+        for (int i = 1; i <= 10; i++) {
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+            System.out.println("scrolling" + i);
+            try {
+                Thread.sleep(2000); // Wait for the page to load
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        String htmlSource = driver.getPageSource();
+        Document doc = Jsoup.parse(htmlSource);
+        Elements links = doc.select("a[href~=^/posts/article]");
+        int ans = 0;
+        for (Element link : links) {
+            ans++;
+            String href = link.attr("href");
+            System.out.println(href);
+        }
+
+        System.out.println(ans);
+
+        driver.quit();
+    }
+}
