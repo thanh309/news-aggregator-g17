428c1c0e99fb8cd64682e0ff0939a102c481dfc0
thanh309 <149485828+thanh309@users.noreply.github.com>
2024-04-07 21:39:33 +0700

    prepare for migration

---
+++ src/main/java/group17/news_aggregator/scraper2/ArticleScraper.java
@@ -0,0 +1,41 @@
+package group17.news_aggregator.scraper2;
+
+import group17.news_aggregator.exception.EmptyContentException;
+import group17.news_aggregator.news.Article;
+import group17.news_aggregator.news.News;
+
+import java.io.IOException;
+import java.util.List;
+
+public abstract class ArticleScraper implements Scraper {
+
+    private int maxPage = 1000;
+
+    @Override
+    public Article scrapeFromURL(String url) {
+        Article article = new Article();
+        try {
+            getInfoFromURL(url, article);
+            return article;
+        } catch (EmptyContentException emptyContentException) {
+            System.out.println(emptyContentException.getMessage());
+        } catch (IOException e) {
+            System.out.println("Error parsing URL: " + url);
+        }
+        return null;
+    }
+
+    @Override
+    public abstract List<Article> scrapeAll() throws InterruptedException;
+
+    @Override
+    public abstract void getInfoFromURL(String url, News news) throws IOException;
+
+    public int getMaxPage() {
+        return maxPage;
+    }
+
+    public void setMaxPage(int maxPage) {
+        this.maxPage = maxPage;
+    }
+}
---
+++ src/main/java/group17/news_aggregator/scraper2/Scraper.java
@@ -0,0 +1,15 @@
+package group17.news_aggregator.scraper2;
+
+import group17.news_aggregator.news.News;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface Scraper {
+    News scrapeFromURL(String url);
+
+    List<? extends News> scrapeAll() throws InterruptedException;
+
+    void getInfoFromURL(String url, News news) throws IOException;
+
+}
