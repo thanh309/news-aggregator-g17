0791b2c8d726f29162df458f656b7d1af28fcea2
your_username <117010350+lht11062004@users.noreply.github.com>
2024-04-11 13:59:47 +0700

    Test search engine

---
+++ src/python/PythonSearchEngine/test_search_engine.py
@@ -0,0 +1,8 @@
+from src.python.PythonSearchEngine.search_from_file import *
+
+if __name__ == "__main__":
+    file_name_csv = input()
+    file_name_json = input()
+
+    search_from_file(file_name_csv, file_name_json)
+
--- src/python/search_test.py
+++
@@ -1,7 +0,0 @@
-import regex as re
-
-if __name__ == "__main__":
-    string = input()
-    pattern = r'bitcoin'
-
-    print(re.findall(pattern, string))
