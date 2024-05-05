3bef3da70094602b493bbc8419082d6071ed876a
thanh309 <149485828+thanh309@users.noreply.github.com>
2024-05-06 00:59:34 +0700

    multiple changes (#15)
    - excluded some unneeded dependencies
    - added a Main wrapper for the application. Hopefully no more error
    - add some configs to build fat jar for standalone executable

---
+++ src/main/java/group17/news_aggregator/gui/Main.java
@@ -0,0 +1,9 @@
+package group17.news_aggregator.gui;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class);
+    }
+}
