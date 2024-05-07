a6357df9f339404b852fc4348c419fefe7630b13
your_username <117010350+lht11062004@users.noreply.github.com>
2024-05-08 00:36:45 +0700

    implement autocomplete in java

---
+++ src/main/java/javasearchengine/autocomplete/TrieNode.java
@@ -0,0 +1,31 @@
+package javasearchengine.autocomplete;
+
+import java.util.*;
+
+public class TrieNode {
+    private Map<Character, TrieNode> children;
+    private boolean checkIsWord;
+
+    public TrieNode() {
+        children = new HashMap<>();
+        checkIsWord = false;
+    }
+
+    public Map<Character, TrieNode> getChildren() {
+        return children;
+    }
+
+    public void setChildren(Map<Character, TrieNode> children) {
+        this.children = children;
+    }
+
+    public boolean getCheckIsWord() {
+        return checkIsWord;
+    }
+
+    public void setCheckIsWord(boolean checkIsWord) {
+        this.checkIsWord = checkIsWord;
+    }
+
+}
+
