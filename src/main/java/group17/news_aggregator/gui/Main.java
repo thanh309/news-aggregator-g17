54397b69c46be763a65190cea679575c21e3f45f
Vũ <149512389+bluff-king@users.noreply.github.com>
2024-05-19 21:12:31 +0700

    Scrape Scene fix fxml

--- src/main/java/group17/news_aggregator/gui/Main.java
+++ src/main/java/group17/news_aggregator/gui/Main.java
@@ -1,10 +1,10 @@
 package group17.news_aggregator.gui;
 
-import group17.news_aggregator.gui.application.HelloApplication;
+import group17.news_aggregator.gui.application.HomeApplication;
 import javafx.application.Application;
 
 public class Main {
     public static void main(String[] args) {
-        Application.launch(HelloApplication.class);
+        Application.launch(HomeApplication.class);
     }
 }
